e3fc8c4d723142df5506a3168cfe103a1e946805
border <border@student.21-school.ru>
2022-02-01 23:00:15 +0300

    добавлена авторизация

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,14 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from sys import platform
 import time
+import os
 
-url = "https://signin.intra.42.fr/users/sign_in"
-chrome_service = Service("/home/border/Desktop/autoSlotCatcher/chromedriver/chromedriver_linux")
-driver = webdriver.Chrome(service=chrome_service)
+url = ""
+login = ""
+password = ""
+
+
+options = webdriver.ChromeOptions()
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36")
+
+abspath = os.path.abspath("chromedriver_linux" if platform == "linux" else "chromedriver_mac")
+chrome_service = Service(abspath)
+driver = webdriver.Chrome(service=chrome_service, options=options)
 
 try:
-    driver.get(url=url)
+    driver.get(url="https://signin.intra.42.fr/users/sign_in")
+    time.sleep(2)
+
+    login_input = driver.find_element(By.ID, "user_login")
+    login_input.clear()
+    login_input.send_keys(login)
+
+    password_input = driver.find_element(By.ID, "user_password")
+    password_input.clear()
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.ENTER)
+
     time.sleep(10)
+
     # driver.refresh() # обновление страницы
 except Exception as ex:
     print(ex)
